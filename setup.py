0d20cd027edd3db06e351b1a2a866e2dd9d916c0
Dan Meshkov <102177425+impreso@users.noreply.github.com>
2022-12-02 23:30:37 -0800

    Update README.md

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='s3-helper',
+    version='0.1.0',
+    packages=['open_rarity', 'scripts'],
+    install_requires=[
+        'numpy==1.24.1 ',
+        'pydantic==1.10.4',
+    ]
+)
