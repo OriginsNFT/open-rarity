d0b864abe1b1c25c8bdc7fa3e8349a77f01e6342
James Galvan <79471955+jamesgalvan-dev@users.noreply.github.com>
2023-01-13 12:40:31 -0800

    move scripts to package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='s3-helper',
     version='0.1.0',
-    packages=['open_rarity', 'scripts'],
+    packages=['open_rarity'],
     install_requires=[
         'numpy==1.24.1 ',
         'pydantic==1.10.4',
